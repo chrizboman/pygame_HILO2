05c6cc2726c77b2a22e21b17cea7ef4bbadd8832
Christoffer <chrizboman@gmail.com>
2023-07-12 23:04:12 +0200

    more things

--- components/components.py
+++ components/components.py
@@ -1,5 +1,21 @@
+import pygame
+from VARS import COLOR
+
 class Button():
-    pass
+    def __init__(self, text: str, pos_x: int, pos_y: int, width: int, height: int, fromCenter=False):
+        self.text = text
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.width = width
+        self.height = height
+    
+    def Draw(self, screen ):
+        pygame.draw.rect(screen, COLOR.WHITE, (self.pos_x, self.pos_y, self.width, self.height))
+        if self.text != '':
+            font = pygame.font.Font(None, 30)
+            text = font.render(self.text, True, COLOR.BLACK)
+            text = font.render(self.text, 1, (0,0,0))
+            screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
 class Text():
     pass
